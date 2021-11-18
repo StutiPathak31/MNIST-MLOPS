a2a473ee3615ae8dfadf8f8f42073e67d354c72a
StutiPathak31 <stutipathak31jan@gmail.com>
2021-11-18 15:11:05 +0530

    SVM vs DecisionTree

--- mnist-mlops/plot_graphs.py
+++ mnist-mlops/plot_graphs.py
@@ -4,7 +4,7 @@ import os
 import matplotlib.pyplot as plt
 
 # Import datasets, classifiers
-from sklearn import datasets, svm
+from sklearn import datasets, svm, tree
 
 from skimage import data, color
 
@@ -13,104 +13,134 @@ import numpy as np
 from joblib import dump, load
 
 from utils import preprocess, create_splits, test
+import pandas as pd
 
-
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
 
 digits = datasets.load_digits()
 
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
-
 parent_directory = '/mnt/c/Users/Stuti Pathak/Desktop/IIT Jodhpur/Semester 3/MLOps/MNIST-MLOPS'
-os.mkdir(parent_directory+'/models')
+if not os.path.exists(parent_directory + '/models'):
+    os.mkdir(parent_directory+'/models')
+parent_directory += '/models'
 # flatten the images
 n_samples = len(digits.images)
 
-# rescale_factors = [0.25, 0.5, 1, 2, 3]
-rescale_factors = [1]
-for test_size, valid_size in [(0.15, 0.15)]:
-    for rescale_factor in rescale_factors:
-        model_candidates = []
-        for gamma in [10 ** exponent for exponent in range(-7, 0)]:
+def findBestModel():
+
+    # rescale_factors = [0.25, 0.5, 1, 2, 3]
+    gammalist = [10 ** exp for exp in range(-7, 0)]
+    max_depth = [5, 6, 7, 8, 9, 10, 20, 30, 40, 80, 100]
+    rescale_factors = [1]
+    models = ['SVM', 'DecisionTree']
+    hyperp = {'SVM':[10 ** exp for exp in range(-7, 0)], 'DecisionTree':[5, 6, 7, 8, 9, 10, 20, 30, 40, 80, 100]}
+    bestModel = {}
+    for test_size, valid_size in [(0.15, 0.15)]:
+        for rescale_factor in rescale_factors:
             resized_images = preprocess(
-                images=digits.images, rescale_factor=rescale_factor
-            )
+                    images=digits.images, rescale_factor=rescale_factor
+                )
             resized_images = np.array(resized_images)
             data = resized_images.reshape((n_samples, -1))
-
-            # Create a classifier: a support vector classifier
-            clf = svm.SVC(gamma=gamma)
             X_train, X_test, X_valid, y_train, y_test, y_valid = create_splits(
-                data, digits.target, test_size, valid_size
-            )
+                    data, digits.target, test_size, valid_size
+                )
 
-            # print("Number of samples: Train:Valid:Test = {}:{}:{}".format(len(y_train),len(y_valid),len(y_test)))
+            for model in models:
+                if not os.path.exists(parent_directory+'/'+ model):
+                        os.mkdir(parent_directory+'/'+ model)
+                model_candidates = []
+                for hp in hyperp[model]:
+                    if model == 'DecisionTree':
+                        clf = tree.DecisionTreeClassifier(max_depth = hp)
+                    elif model == 'SVM':
+                        clf = svm.SVC(gamma = hp)
 
-            # Learn the digits on the train subset
-            clf.fit(X_train, y_train)
-            metrics_valid = test(clf, X_valid, y_valid)
+                    clf.fit(X_train, y_train)
+                    metrics_valid = test(clf, X_valid, y_valid)
+                
+                    candidate = {
+                        "acc_valid": metrics_valid['acc'],
+                        "f1_valid": metrics_valid['f1'],
+                        "hyperp": hp,
+                        }
+                    #print(candidate)
+                    if metrics_valid['acc'] < 0.11:
+                        #print("Skipping for {}".format(hp))
+                        continue
+
+                    model_candidates.append(candidate)
+                    output_folder = parent_directory+"/"+model+"/tt_{}_val_{}_rescale_{}_hyperp_{}".format(
+                        test_size, valid_size, rescale_factor, hp
+                        )
+                    if not os.path.exists(output_folder):
+                        os.mkdir(output_folder)
+                    dump(clf, os.path.join(output_folder, "model.joblib"))
+
+            # Predict the value of the digit on the test subset
+
+                max_valid_f1_model_candidate = max(
+                    model_candidates, key=lambda x: x["f1_valid"]
+                    )
+                best_model_folder = parent_directory+"/"+model+"/tt_{}_val_{}_rescale_{}_hyperp_{}".format(
+                    test_size, valid_size, rescale_factor, max_valid_f1_model_candidate['hyperp']
+                    )
+                clf = load(os.path.join(best_model_folder, "model.joblib"))
+
+                metrics = test(clf, X_test, y_test)
+                #print(
+                #    "{}x{}\t{}\t{}:{}\t{:.3f}\t{:.3f}".format(
+                #        resized_images[0].shape[0],
+                #        resized_images[0].shape[1],
+                #        max_valid_f1_model_candidate["hyperp"],
+                #        (1 - test_size) * 100,
+                #        test_size * 100,
+                #        metrics['acc'],
+                #        metrics['f1'],
+                #    )
+                #)
+                bestModel[model] = {'hyperparameter':max_valid_f1_model_candidate['hyperp'], 'acc':metrics['acc'], 'f1':metrics['f1']}
+    return bestModel
+
+def runMultiple():
+    df = pd.DataFrame(columns=['Run', 'TreeDepth', 'TreeAcc', 'TreeF1', 'SVMGamma', 'SVMAcc', 'SVMF1'])
+    for i in range(5):
+        param = findBestModel()
+        temp = {}
+        temp['Run'] = int(i + 1)
+        temp['TreeDepth'] = param['DecisionTree']['hyperparameter']
+        temp['TreeAcc'] = param['DecisionTree']['acc']
+        temp['TreeF1'] = param['DecisionTree']['f1']
+        temp['SVMGamma'] = param['SVM']['hyperparameter']
+        temp['SVMAcc'] = param['SVM']['acc']
+        temp['SVMF1'] = param['SVM']['f1']
+        #print(temp)
+        df = df.append(temp, ignore_index = True)
+
+    dfmean = df.mean()
+    dfmean['Run'] = 'Mean'
+    dfstd = df.std()
+    dfstd['Run'] = 'StdDev'
+    df = df.append(dfmean, ignore_index = True)
+    df = df.append(dfstd, ignore_index = True)
+    df['TreeDepth'][5] = ""
+    df['TreeDepth'][6] = ""
+    df['SVMGamma'][5] = ""
+    df['SVMGamma'][6] = ""
+    print(df)
+
+
+runMultiple()
+        
+
+
+
+
+
+
+
+
+
+
             
-            # we will ensure to throw away some of the models that yield random-like performance.
-            if metrics_valid['acc'] < 0.11:
-                print("Skipping for {}".format(gamma))
-                continue
 
-            candidate = {
-                "acc_valid": metrics_valid['acc'],
-                "f1_valid": metrics_valid['f1'],
-                "gamma": gamma,
-            }
-            model_candidates.append(candidate)
-            output_folder = parent_directory+"/models/tt_{}_val_{}_rescale_{}_gamma_{}".format(
-                test_size, valid_size, rescale_factor, gamma
-            )
-            os.mkdir(output_folder)
-            dump(clf, os.path.join(output_folder, "model.joblib"))
-
-        # Predict the value of the digit on the test subset
-
-        max_valid_f1_model_candidate = max(
-            model_candidates, key=lambda x: x["f1_valid"]
-        )
-        best_model_folder = parent_directory+"/models/tt_{}_val_{}_rescale_{}_gamma_{}".format(
-            test_size, valid_size, rescale_factor, max_valid_f1_model_candidate["gamma"]
-        )
-        clf = load(os.path.join(best_model_folder, "model.joblib"))
-
-        metrics = test(clf, X_test, y_test)
-        print(
-            "{}x{}\t{}\t{}:{}\t{:.3f}\t{:.3f}".format(
-                resized_images[0].shape[0],
-                resized_images[0].shape[1],
-                max_valid_f1_model_candidate["gamma"],
-                (1 - test_size) * 100,
-                test_size * 100,
-                metrics['acc'],
-                metrics['f1'],
-            )
-        )
+            
